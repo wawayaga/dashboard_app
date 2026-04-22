f19807644beaf7178cdf53fe062cbc8d88577485
Mariana <mlete@proton.me>
2026-04-22 20:57:42 +0200

    Adds validation error messages in create form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -10,9 +10,15 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+    amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
     date: z.string(),
 });
 
@@ -20,27 +26,64 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData){
-    const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+  values?: {
+    customerId?: string;
+    amount?: string;
+    status?: string;
+  };
+};
+
+export async function createInvoice(prevState: State, formData: FormData){
+  //safeParse() will return an object containing either a success or error field.
+  // This will help handle validation more gracefully without having put this logic inside the try/catch block.
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    console.log(`validated fields is ${validatedFields}`);
+    console.log(`validated fields errors is ${validatedFields.error}`);
+    console.log(`validatedFields.success is ${validatedFields.success}`);
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+      values: {
+        customerId: formData.get('customerId') as string,
+        amount: formData.get('amount') as string,
+        status: formData.get('status') as string,
+      },
+    };
+  }
+ 
+  // Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
-
+  // Insert data into the database
     try{
     await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
     } catch (error) {
+    // If a database error occurs, return a more specific error.
       console.error(error);
       return {
         message:'Database Error: Failed to Create Invoice'
       };
     }
-
+  // Revalidate the cache for the invoices page and redirect the user.
     revalidatePath('/dashboard/invoices');
     redirect ('/dashboard/invoices');
 }
@@ -73,7 +116,13 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
+  try{
     await sql`DELETE FROM invoices WHERE ID = ${id}`;
     revalidatePath('/dashboard/invoices');
+  } catch(error){
+    console.log(error);
+    return{
+      message:'Database Error. Failed to Delte Invoice'
+    };
+  }
 }
